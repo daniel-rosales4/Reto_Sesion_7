6f94071b21a8139b883e3befa4d4a3efb46fdf3f
daniel_rosales4 <jdrr2601@gmail.com>
2022-08-11 02:47:20 +0000

    Carga de script Reto 1 Sesion 7

---
+++ queries.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(DBI)
+library(RMySQL)
+library(ggplot2)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+dbListTables(MyDataBase)
+
+dbListFields(MyDataBase, 'CountryLanguage')
+DataDB <- dbGetQuery(MyDataBase, "select * from CountryLanguage where Language = 'Spanish'")
+head(DataDB)
+
+ggplot(DataDB, aes(x=Percentage, y = CountryCode, colour = IsOfficial )) + 
+  geom_point() +   
+  xlab('Porcentaje') +
+  ylab('País')
